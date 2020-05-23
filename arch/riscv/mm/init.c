644870323dd2d8c84f3ec1a58e84e5ac65024590
Liu Yibin <jiulong@linux.alibaba.com>
2020-05-23 10:43:09 +0800

    Flush TLB after set_pte()
    Flush TLB after any kind of page table operation
    
    Signed-off-by: Liu Yibin <jiulong@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>

--- arch/riscv/mm/init.c
+++ arch/riscv/mm/init.c
@@ -171,8 +171,8 @@ void __set_fixmap(enum fixed_addresses idx, phys_addr_t phys, pgprot_t prot)
 		set_pte(ptep, pfn_pte(phys >> PAGE_SHIFT, prot));
 	} else {
 		pte_clear(&init_mm, addr, ptep);
-		local_flush_tlb_page(addr);
 	}
+	local_flush_tlb_page(addr);
 }
 
 static pte_t *__init get_pte_virt(phys_addr_t pa)
