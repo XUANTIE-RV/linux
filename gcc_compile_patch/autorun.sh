32a26144153be4a50008e73efbec88652710a0a0
lihm <koumei.lhm@alibaba-inc.com>
2020-10-10 09:40:48 +0800

    Modify autorun.sh script miss.
    Change-Id: Ida712c22cdee5f4fd55fa5db128b938a68ba5bf8

--- gcc_compile_patch/autorun.sh
+++ gcc_compile_patch/autorun.sh
@@ -2,18 +2,18 @@
 
 echo "Check patch..."
 git apply --check gcc_compile_patch/0001-Modify-to-compile-by-gcc.patch
-echo "Check patch OK\n"
+echo -e "Check patch OK\n"
 
 echo "Apply patch..."
 git am gcc_compile_patch/0001-Modify-to-compile-by-gcc.patch
-echo "Apply patch OK\n"
+echo -e "Apply patch OK\n"
 
 echo "Install toolchain..."
 wget https://occ-oss-prod.oss-cn-hangzhou.aliyuncs.com/resource/1836682/1600327810715/riscv64-linux-x86_64-20200820.tar.gz
 mkdir -p host
 tar zxf riscv64-linux-x86_64-20200820.tar.gz -C host
 rm riscv64-linux-x86_64-20200820.tar.gz
-echo "Install toolchain OK\n"
+echo -e "Install toolchain OK\n"
 
 export LD_LIBRARY_PATH=host/lib
 export ARCH=riscv
