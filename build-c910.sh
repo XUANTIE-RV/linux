9f1eafe58bf08482508139a69e839cdf39928782
Ma Jun <majun258@linux.alibaba.com>
2021-02-05 10:00:01 +0800

    build-c910.sh: Update the compile script file
    Signed-off-by: Ma Jun <majun258@linux.alibaba.com>

--- build-c910.sh
+++ build-c910.sh
@@ -7,9 +7,12 @@
 #               build-gcc.sh dtbs
 export PATH=$PATH:/home/majun/ice-910/sdk-toolchain
 
-make  $1  -j16 ARCH=riscv  CROSS_COMPILE=/home/majun/ice-910/sdk-toolchain/riscv64-unknown-linux-gnu-
+make  LOCALVERSION= $1  -j64 ARCH=riscv  CROSS_COMPILE=/home/majun/ice-910/sdk-toolchain/riscv64-unknown-linux-gnu-
 
 if [ "$1" == 'Image' ]; then
     echo "start to generate the uImage"
     ../../tools/mkimage  -A riscv -O linux -T kernel -C none -a 0x00200000 -e 0x00200000 -n Linux -d ./arch/riscv/boot/Image ./uImage
+elif [ "$1" == 'savedefconfig' ]; then
+    echo "save the defconfig as gki_defconfig"
+    mv defconfig arch/riscv/configs/gki_defconfig
 fi
